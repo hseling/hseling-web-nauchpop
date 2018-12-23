1bdadc15350b6f8f81d0de5f329a6cd5dc1cfc84
ana-kuznetsova <menina.indigena.17@gmail.com>
2018-12-23 19:29:02 +0300

    Add forms and new templates

--- src/web/views.py
+++ src/web/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
 from django import forms
 
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 
 HSE_API_ROOT = "http://hse-api-web/"
@@ -12,9 +16,20 @@ def web_index(request):
     return render(request, 'index.html',
                   context={})
 
+def web_about(request):
+    return render(request, 'about.html',
+                  context={})
+
+def web_documentation(request):
+    return render(request, 'documentation.html',
+                  context={})
+
+def web_contact(request):
+    return render(request, 'contact.html',
+                  context={})
+
 
 def web_main(request):
-    # content = requests.get(HSE_API_ROOT)
     return render(request, 'main.html',
                   context={"status": request.GET.get('status')})
 
@@ -31,6 +46,19 @@ def web_status(request):
         return JsonResponse(result)
     return JsonResponse({"error": "No task id"})
 
+class ProcessTextForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea, required=False)
+    ner = forms.BooleanField(required=False)
+    term_extraction = forms.BooleanField(required=False)
+    text_classification = forms.BooleanField(required=False)
+    readability = forms.BooleanField(required=False)
+
+def web_methods(request):
+    if request.method == 'POST':
+        form = ProcessTextForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+    return render(request, 'methods.html', {'form':form})
 
 def handle_uploaded_file(f):
     files = {'file': f}
@@ -44,13 +72,12 @@ def handle_uploaded_file(f):
         content = requests.get(url)
 
     else:
-        raise Exception(content.json())
-
-    return content.json().get('task_id')
+        return content.json().get('task_id')
 
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
 
 
 def web_upload_file(request):
