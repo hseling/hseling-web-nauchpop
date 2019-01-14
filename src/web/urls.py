8f44ca0e8656d0205ec9568dbae0230d68fbbe6b
semenovabnl <semenova.bnl@gmail.com>
2019-01-15 02:17:19 +0300

    update

--- src/web/urls.py
+++ src/web/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.web_index, name='index'),
     url(r'^main$', views.web_main, name='main'),
     url(r'^upload$', views.web_upload_file, name='upload'),
+    url(r'^type_in', views.web_type_in, name='type_in'),
     url(r'^status$', views.web_status, name='status'),
     url(r'^about$', views.web_about, name='about'),
     url(r'^documentation$', views.web_documentation, name='documentation'),
--- src/web/views.py
+++ src/web/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
 from .forms import UploadFileForm
-from .forms import ProcessTextForm
+from .forms import TypeInTextForm
+from django.views.decorators.csrf import csrf_protect
+# from .forms import FileFieldForm
+# from django.views.generic.edit import FormView
 
 import requests
 import logging
@@ -12,42 +15,35 @@ logger = logging.getLogger(__name__)
 
 HSE_API_ROOT = "http://hse-api-web/"
 
-def select_methods_string(ner, term_extraction, text_classification, readability):
-    methods = []
-    if ner == True:
-        methods.append('ner')
-    if term_extraction == True:
-        methods.append('term')
-    if text_classification == True:
-        methods.append('topic')
-    if readability == True:
-        methods.append('rb')
-    
-    return ', '.join(methods)
 
-def save_user_text(text):
-    with open( HSE_API_ROOT + 'user_text.txt', 'w') as fo:
-        fo.write(text)
-
+# def save_user_text(text):
+#     # with open( HSE_API_ROOT + 'user_text.txt', 'w') as fo:
+#     with open('/opt/code/tmp/user_text.txt', 'w') as fo:
+#         fo.write(text)
 def web_index(request):
-    form = ProcessTextForm()
-    if request.method == 'POST':
-        form = ProcessTextForm(request.POST)
-        if form.is_valid():
-            save_user_text(form.cleaned_data['text'])
-            ner = form.cleaned_data['ner']
-            term_extraction = form.cleaned_data['term_extraction']
-            text_classification = form.cleaned_data['text_classification']
-            readability = form.cleaned_data['readability']
-            methods = select_methods_string(ner, term_extraction, 
-                                            text_classification, readability)
-            if text:
-                post_form_data(methods)
-    return render(request, 'index.html', 
-            context={'form':form})
+    return render(request, 'index.html',
+                  context={})
+# def web_index(request):
+#     form = ProcessTextForm()
+#     if request.method == 'POST':
+#         form = ProcessTextForm(request.POST)
+#         if form.is_valid():
+#             # save_user_text(form.cleaned_data['text'])
+#             ner = form.cleaned_data['ner']
+#             term_extraction = form.cleaned_data['term_extraction']
+#             text_classification = form.cleaned_data['text_classification']
+#             readability = form.cleaned_data['readability']
+#             methods = select_methods_string(ner, term_extraction,
+#                                             text_classification, readability)
+#
+#             post_form_data(methods)
+#     return render(request, 'index.html',
+#             context={'form':form})
 
-def post_form_data(methods):
-    return requests.post(url=HSE_API_ROOT + 'process', data=methods)
+
+
+# def post_form_data(methods):
+#     return requests.post(url=HSE_API_ROOT + 'process', data=methods)
 
 def web_about(request):
     return render(request, 'about.html',
@@ -76,24 +72,82 @@ def web_status(request):
         if result.get('status') == 'SUCCESS':
             content = requests.get(HSE_API_ROOT + 'files/' + result.get('result', [""])[0])
             result['raw'] = content.content.decode('utf-8')
-        return JsonResponse(result)
+            return JsonResponse(result)
     return JsonResponse({"error": "No task id"})
 
-def handle_uploaded_file(f):
+
+def handle_uploaded_file(f, modules):
+
     files = {'file': f}
     url = HSE_API_ROOT + "upload"
     content = requests.post(url, files=files)
     file_id = content.json().get("file_id")
-    return file_id
+
+    if file_id:
+        file_id = file_id[7:]
+        url = HSE_API_ROOT + "process/" + file_id
+        content = requests.post(url, data=modules)
 
 
+    else:
+        raise Exception(content.json())
+    response = list(content.json().values())
 
+    return response
+
+# def web_process_file(request):
+
+@csrf_protect
 def web_upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            task_id = handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('main?task_id=' + task_id)
+            modules = list(filter(lambda t: t[0] in form.cleaned_data['modules'], form.fields['modules'].choices))
+            modules = [f[0] for f in modules]
+            modules = ','.join(modules)
+            task_ids = handle_uploaded_file(request.FILES['file'], modules)
+            task_ids = '&'.join(task_ids)
+            return HttpResponseRedirect('main?task_id=' + str(task_ids))
     else:
         form = UploadFileForm()
     return render(request, 'main.html', {'form_upload': form})
+
+def web_type_in(request):
+    if request.method == 'POST':
+        form = TypeInTextForm(request.POST, request.FILES)
+        if form.is_valid():
+            modules = list(filter(lambda t: t[0] in form.cleaned_data['modules'], form.fields['modules'].choices))
+            modules = [f[0] for f in modules]
+            modules = ','.join(modules)
+            file = open('test.txt', 'rb+')
+            subject = form.cleaned_data['text']
+            subject = bytes(subject, encoding='utf-8')
+            file.write(subject)
+            # file.close()
+            task_ids = handle_uploaded_file(file, modules)
+            file.close()
+
+            return HttpResponseRedirect('main?task_id=' + str(task_ids))
+    else:
+        form = TypeInTextForm()
+    return render(request, 'main.html', {'form_text': form})
+
+
+# class FileFieldView(FormView):
+#     form_class = FileFieldForm
+#     template_name = 'main.html'  # Replace with your template.
+#     success_url = 'main' # Replace with your URL or reverse().
+#
+#     def post(self, request, *args, **kwargs):
+#         form_class = self.get_form_class()
+#         form = self.get_form(form_class)
+#         files = request.FILES.getlist('file_field')
+#         if form.is_valid():
+#             for f in files:
+#                 task_id = handle_uploaded_file(f)
+#
+#                 ...  # Do something with each file.
+#             return self.form_valid(form)
+#         else:
+#
+#             return self.form_invalid(form)
