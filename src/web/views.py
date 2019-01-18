f994fa68c067dfbab62241fce8f09f235d96e59b
ana-kuznetsova <menina.indigena.17@gmail.com>
2019-01-18 12:12:27 +0300

    Change form structure

--- src/web/views.py
+++ src/web/views.py
@@ -131,23 +131,3 @@ def web_type_in(request):
     else:
         form = TypeInTextForm()
     return render(request, 'main.html', {'form_text': form})
-
-
-# class FileFieldView(FormView):
-#     form_class = FileFieldForm
-#     template_name = 'main.html'  # Replace with your template.
-#     success_url = 'main' # Replace with your URL or reverse().
-#
-#     def post(self, request, *args, **kwargs):
-#         form_class = self.get_form_class()
-#         form = self.get_form(form_class)
-#         files = request.FILES.getlist('file_field')
-#         if form.is_valid():
-#             for f in files:
-#                 task_id = handle_uploaded_file(f)
-#
-#                 ...  # Do something with each file.
-#             return self.form_valid(form)
-#         else:
-#
-#             return self.form_invalid(form)
